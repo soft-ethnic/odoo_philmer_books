2f803fdbcdfbe7d207988797ac2c60c8a0cc6c44
Soft Ethnic <dev@soft-ethnic.be>
2016-06-05 18:21:45 +0200

    better description

--- philmer_books_bibliophilia/__openerp__.py
+++ philmer_books_bibliophilia/__openerp__.py
@@ -3,7 +3,7 @@
     'name': u"Import of Bibliophilia backups files into Philmer's Books",
     'summary': "BiblioPhilia is perfect on iPhone to get quick data about books.\nPhilmer's Books is perfect to manage my library.\nThis module links them ...",
     'description':"""
-This module permit the import of biblioPhilia backup files into Philmer's Books. Manage the import of the same backup again and again. Manage the link betweek Philmer's books and Bibliophilia's records'
+This module permit the import of biblioPhilia backup files into Philmer's Books.\nManage the import of the same backup again and again.\nManage the link betweek Philmer's books and Bibliophilia's records'
 """,
     'depends': ['base','philmer_books'],
     'data': ['views/philmer_books_imported_ids_view.xml',],
